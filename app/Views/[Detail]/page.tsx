0b1b6cbebc7b9459b2fef8b568caa645c1b21de6
Francisco Borges <50386937+fran22345@users.noreply.github.com>
2023-11-06 09:42:28 -0300

    Update page.tsx

--- app/Views/[Detail]/page.tsx
+++ app/Views/[Detail]/page.tsx
@@ -32,7 +32,7 @@ function Detail() {
   const stripePayment = async () => {
     
       try {                
-        const response = await fetch('http://localhost:3001/payment/createStripeCS', {
+        const response = await fetch('https://nest-refj.onrender.com/payment/createStripeCS', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
